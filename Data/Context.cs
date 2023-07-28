4da88e2dd7d22cd303abdfeda21e26e93e7958dc
Hygor <Hygor@DESKTOP-55BA59J>
2023-07-28 19:05:33 -0300

    Criação do contexto

---
+++ Data/Context.cs
@@ -0,0 +1,21 @@
+﻿using ImobiliariaLM.RazorPages.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImobiliariaLM.RazorPages.Data;
+
+public class Contexto : DbContext
+{
+    public DbSet<CadastroModel> Cadastro { get; set; }
+
+    public Contexto(DbContextOptions<Contexto> opcoes) : base(opcoes)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<CadastroModel>()
+            .HasKey(t => t.Id)
+            .HasAnnotation("Sqlite:Autoincrement", true);
+    }
+}
