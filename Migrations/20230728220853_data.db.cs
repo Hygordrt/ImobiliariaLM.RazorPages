9afc9c15d9312dde5e600d9d8d3cef6e3e67cc14
Hygor <Hygor@DESKTOP-55BA59J>
2023-07-28 19:12:52 -0300

    migration e update-database

---
+++ Migrations/20230728220853_data.db.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ImobiliariaLM.RazorPages.Migrations
+{
+    /// <inheritdoc />
+    public partial class datadb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cadastro",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Titulo = table.Column<string>(type: "TEXT", nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: false),
+                    Valor = table.Column<string>(type: "TEXT", nullable: false),
+                    Categoria = table.Column<string>(type: "TEXT", nullable: false),
+                    VendaAluguel = table.Column<string>(type: "TEXT", nullable: false),
+                    CEP = table.Column<string>(type: "TEXT", nullable: true),
+                    Endereco = table.Column<string>(type: "TEXT", nullable: true),
+                    Cidade = table.Column<string>(type: "TEXT", nullable: false),
+                    Bairro = table.Column<string>(type: "TEXT", nullable: false),
+                    Complemento = table.Column<string>(type: "TEXT", nullable: false),
+                    MtQuadrado = table.Column<string>(type: "TEXT", nullable: false),
+                    QtdBanheiros = table.Column<string>(type: "TEXT", nullable: false),
+                    QtdQuartos = table.Column<string>(type: "TEXT", nullable: false),
+                    QtdVagas = table.Column<string>(type: "TEXT", nullable: false),
+                    NomeImagemImovel = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cadastro", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cadastro");
+        }
+    }
+}
