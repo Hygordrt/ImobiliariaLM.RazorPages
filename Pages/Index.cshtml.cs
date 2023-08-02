01cd73dcd71734808e23f3d156c8cc5068dd5a5d
Hygor <Hygor@DESKTOP-55BA59J>
2023-08-02 17:04:16 -0300

    - Layout da home - Recuperar dados do banco

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ImobiliariaLM.RazorPages.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ImobiliariaLM.RazorPages.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ImobiliariaLM.RazorPages.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly Contexto _contexto;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(Contexto contexto, ILogger<IndexModel> logger)
         {
             _logger = logger;
+            _contexto = contexto;
         }
 
-        public void OnGet()
-        {
+        public List<CadastroModel> Dados { get; set; }
 
+        public async Task OnGet()
+        {
+            Dados = await _contexto.Cadastro.ToListAsync();
         }
     }
 }
